babf9bcb2e4ac9d4ccbc9827c558bfffa96f06a8
kriyes <kriyes@gmail.com>
2020-08-25 19:40:15 +0530

    add database settings

--- act/settings.py
+++ act/settings.py
@@ -63,17 +63,24 @@ WSGI_APPLICATION = 'act.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'durga2',
-        'USER': 'durga',
-        'PASSWORD': 'durga98765durga@@@',
-        'HOST': 'localhost',
-        'PORT': '',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'durga2',
+            'USER': 'durga',
+            'PASSWORD': 'durga98765durga@@@',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
