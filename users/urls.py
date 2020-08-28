ab4c6e3ae4e0249fcc4a1e7db62066ae507bcd80
kriyes <kriyes@gmail.com>
2020-08-28 13:54:54 +0530

    updated durga html & add login and logout

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 
 from .views import *
 
-app_name = "users"
+app_name="users"
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name="register"),
+    path('register/',RegisterView.as_view(),name="register"),
+    path('login/',UserLoginView.as_view(),name="login"),
+    path('logout/',UserLogoutView.as_view(),name="logout"),
+
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
+
 # Create your views here.
 from django.views.generic import CreateView
-
 from .forms import RegisterForm
+
 
 
 class RegisterView(CreateView):
     template_name = 'users/register.html'
     form_class = RegisterForm
     success_url = '/'
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+
+
+class UserLogoutView(LogoutView):
+    template_name = 'users/logout.html'
